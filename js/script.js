7594a01806d147028fc9f08f82e59bebea57c013
coldwhiskeyman <eldiablo5783@gmail.com>
2020-12-01 15:21:20 +0300

    artist dropdown list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(function() {
 	$('.burger').on('click', function() {
 		$('.mobile-nav').toggleClass('closed')
-	})
+	});
+
+	$('.styles-list__item').on('click', function(event) {
+		$(this).find('.artist-dropdown').toggleClass('closed');
+		$(this).find('.styles-list__link::after').animate({transform: '+=rotate(-180deg)'}); // не работает
+	});
 });
