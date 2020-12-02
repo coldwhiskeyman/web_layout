b213237a93ec0a8a66641cff0e2a6a65c4430d68
coldwhiskeyman <eldiablo5783@gmail.com>
2020-12-02 13:24:50 +0300

    dropdown scrollbar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 $(function() {
+	function showModal(element) {
+		element.removeClass('closed');
+		setTimeout(function(){
+			element.removeClass('hidden');
+		}, 10);
+	};
+
+	function hideModal(element) {
+		element.addClass('hidden');
+		setTimeout(function(){
+			element.addClass('closed');
+		}, 500);
+	};
+
 	$('.burger').on('click', function() {
 		$('.mobile-nav').toggleClass('closed')
 	});
 
 	$('.styles-list__item').on('click', function(event) {
-		$(this).find('.artist-dropdown').toggleClass('closed');
+		selfDropdown = $(this).find('.artist-dropdown')
+
+		if (selfDropdown.hasClass('closed')) {
+			showModal(selfDropdown);
+		} else {
+			hideModal(selfDropdown)
+		};
 		$(this).find('.styles-list__link::after').animate({transform: '+=rotate(-180deg)'}); // не работает
 	});
 });
